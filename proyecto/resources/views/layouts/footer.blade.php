e52dafc335ca79a6edf61f06db5e20b5d7bdd2cc
SebastianCristi <S.cristicastillo@gmail.com>
2017-12-09 22:54:10 -0300

    Se refactorea el dashboard dividiendo los componentes y creando y moviendo cada elemento a sus archivos respectivos por los elementos en comun.

---
+++ proyecto/resources/views/layouts/footer.blade.php
@@ -0,0 +1,33 @@
+<footer class="footer">
+
+</footer>
+
+
+<script src="{{URL::asset('js/jquery-3.2.1.min.js')}}" type="text/javascript"></script>
+<script src="{{URL::asset('js/bootstrap.min.js')}}" type="text/javascript"></script>
+<script src="{{URL::asset('js/material.min.js')}}" type="text/javascript"></script>
+<!--  Charts Plugin -->
+<script src="{{URL::asset('js/chartist.min.js')}}"></script>
+<!--  Dynamic Elements plugin -->
+<script src="{{URL::asset('js/arrive.min.js')}}"></script>
+<!--  PerfectScrollbar Library -->
+<script src="{{URL::asset('js/perfect-scrollbar.jquery.min.js')}}"></script>
+<!--  Notifications Plugin    -->
+<script src="{{URL::asset('js/bootstrap-notify.js')}}"></script>
+<!--  Google Maps Plugin    -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+<!-- Material Dashboard javascript methods -->
+<script src="{{URL::asset('js/material-dashboard.js?v=1.2.0')}}"></script>
+<!-- Material Dashboard DEMO methods, don't include it in your project! -->
+<script src="{{URL::asset('js/demo.js')}}"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+
+        // Javascript method's body can be found in assets/js/demos.js
+        demo.initDashboardPageCharts();
+
+    });
+</script>
+
+
+</html>
--- proyecto/resources/views/layouts/includes.blade.php
+++ proyecto/resources/views/layouts/includes.blade.php
@@ -1,8 +1,16 @@
 
 <!-- Sección CSS  -->
 <link rel="stylesheet" type="text/css" href="#">
-
-
+<link href="{{URL::asset('css/bootstrap.min.css')}}" rel="stylesheet" />
+<link href="{{URL::asset('css/material-dashboard.css?v=1.2.0')}}" rel="stylesheet" />
+<link href="{{URL::asset('css/demo.css')}}" rel="stylesheet" />
+<link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet' type='text/css'>
 
 <!-- Sección JS  -->
 <script type="text/javascript" src="#"></script>
+
+
+
+    
+    
